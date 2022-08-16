b3bfe823671d0b7b5365ec07ec294aa4ea8cea59
Abdella <abdella.shukur2@gmail.com>
2022-08-16 11:51:21 -0400

    typescript lab 1

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 function prepend(numSpaces: number, inputString: string) {
-  // your code here
+  let spaces: string = "";
+  for(let x=0; x<numSpaces;x++){
+  spaces +=  "&nbsp;";
+  }
+  return spaces + inputString;
 }
+
+console.log(prepend(3, "Hello"));
