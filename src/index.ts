075b4bbddbd58940e1ba70eef528e09c9d0b2385
Abdella <abdella.shukur2@gmail.com>
2022-08-16 11:53:29 -0400

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 function prepend(numSpaces: number, inputString: string) {
-  let spaces: string = "";
-  for(let x=0; x<numSpaces;x++){
-  spaces +=  "&nbsp;";
-  }
+    let spaces: string = "";
+    for(let x=0; x<numSpaces;x++){
+       spaces +=  "&nbsp";
+    }
   return spaces + inputString;
 }
 
