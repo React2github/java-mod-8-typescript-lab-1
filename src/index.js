21c3cf0e8bb4e583d7b24531b0f4476bfe2e24b1
Abdella <abdella.shukur2@gmail.com>
2022-08-16 12:49:58 -0400

    added lab 2 changes here too

--- src/index.js
+++
@@ -1,8 +0,0 @@
-function prepend(numSpaces, inputString) {
-    var spaces = "";
-    for (var x = 0; x < numSpaces; x++) {
-        spaces += "&nbsp;";
-    }
-    return spaces + inputString;
-}
-console.log(prepend(3, "Hello"));
